e65b357ff44b52dd82cb62674a161dcdc691b3db
Alassane NIANG <adniang75@gmail.com>
2022-01-21 18:19:00 +0100

    Refactoring CalculatorTest

--- src/test/java/com/alassaneniang/calculator/CalculatorTest.java
+++ src/test/java/com/alassaneniang/calculator/CalculatorTest.java
@@ -1,52 +1,80 @@
 package com.alassaneniang.calculator;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName( "Reverse Polish Calculator Tests" )
+@TestClassOrder( ClassOrderer.OrderAnnotation.class )
 class CalculatorTest {
 
     Calculator calculator;
+    BigDecimal value1;
+    BigDecimal value2;
+    BigDecimal value3;
 
     @BeforeEach
     void setUp () {
         calculator = new Calculator();
+        value1 = BigDecimal.valueOf( 12 );
+        value2 = BigDecimal.valueOf( 22 );
+        value3 = BigDecimal.valueOf( 52 );
     }
 
-    @Test
-    @DisplayName( "New calculator has an accumulator of zero" )
-    void testNewCalculatorHasAnAccumulatorOfZer () {
-        assertThat( calculator.getAccumulator() ).isEqualTo( BigDecimal.ZERO );
+    @Nested
+    @DisplayName( "New calculator" )
+    @Order( 1 )
+    class NewCalculatorTest {
+
+        @Test
+        @DisplayName( "New calculator has an accumulator of zero" )
+        void testNewCalculatorHasAnAccumulatorOfZero () {
+            assertThat( calculator.getAccumulator() ).isEqualTo( BigDecimal.ZERO );
+        }
+
+        @Test
+        @DisplayName( "Setting the value of an accumulator" )
+        void testSettingAccumulatorValue () {
+            calculator.setAccumulator( value1 );
+            assertThat( calculator.getAccumulator() ).isEqualTo( value1 );
+        }
     }
 
-    @Test
-    @DisplayName( "Setting the value of an accumulator" )
-    void testSettingAccumulatorValue () {
-        BigDecimal value = BigDecimal.valueOf( 23 );
-        calculator.setAccumulator( value );
-        assertThat( calculator.getAccumulator() ).isEqualTo( value );
+    @Nested
+    @DisplayName( "Calculator with 3 values" )
+    @Order( 2 )
+    class CalculatorWithThreeValuesTest {
+
+        @BeforeEach
+        void setUp () {
+            calculator.setAccumulator( value1 );
+            calculator.enter();
+            calculator.setAccumulator( value2 );
+            calculator.enter();
+            calculator.setAccumulator( value3 );
+        }
+
+        @Test
+        @DisplayName( "Accumulator after pushing 3 values" )
+        void testAccumulatorAfterPushingThreeValues () {
+            assertThat( calculator.getAccumulator() ).isEqualTo( value3 );
+        }
+
+        @Test
+        void testAccumulatorAfterASingleDrop () {
+            calculator.drop();
+            assertThat( calculator.getAccumulator() ).isEqualTo( value2 );
+        }
+
+        @Test
+        void testAccumulatorAfterATwoDrop () {
+            calculator.drop();
+            calculator.drop();
+            assertThat( calculator.getAccumulator() ).isEqualTo( value1 );
+        }
+
     }
 
-    @Test
-    @DisplayName( "Entering and dropping values" )
-    void testEnteringAndDroppingValues () {
-        BigDecimal value1 = BigDecimal.valueOf( 12 );
-        BigDecimal value2 = BigDecimal.valueOf( 22 );
-        BigDecimal value3 = BigDecimal.valueOf( 52 );
-        calculator.setAccumulator( value1 );
-        calculator.enter();
-        calculator.setAccumulator( value2 );
-        calculator.enter();
-        calculator.setAccumulator( value3 );
-        assertThat( calculator.getAccumulator() ).isEqualTo( value3 );
-        calculator.drop();
-        assertThat( calculator.getAccumulator() ).isEqualTo( value2 );
-        calculator.drop();
-        assertThat( calculator.getAccumulator() ).isEqualTo( value1 );
-    }
 }
